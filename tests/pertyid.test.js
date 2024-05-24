d3c26a9e700c747621f77a5d4e5a43eaacb05230
Aron Høyer <aron@hoyer.com>
2024-05-25 01:29:25 +0200

    create base64url id with node built-ins

--- tests/pertyid.test.js
+++ tests/pertyid.test.js
@@ -1,28 +1,17 @@
-const { expect } = require('chai');
+const { describe, it } = require('node:test');
+const assert = require('node:assert');
 
-const pertyid = require('../index');
+const { pertyid, DEFAULT_LENGTH } = require('../index');
 
 describe('pertyid', () => {
-  it('should generate a pertyid', () => {
-    expect(pertyid()).to.match(/[a-zA-Z0-9_-]{10}/g);
-  });
-
   it('should generate a base64 string of a default length', () => {
-    expect(pertyid().length).to.equal(10);
+    assert.strictEqual(pertyid().length, DEFAULT_LENGTH);
   });
 
   it('should generate a base64 string of a given length', () => {
-    [15, 32, 6, 11, 87, 17, 1, 5, 3].forEach((len) => expect(pertyid(len).length).to.equal(len));
-  });
-
-  it('should not generate a base64 string with invalid length argument', () => {
-    try {
-      expect(pertyid('not a number'));
-    } catch (err) {
-      expect(err.name).to.equal('ERR_INVALID_ARGS');
-      return true;
-    }
-
-    throw new Error('should not generate a base64 string with invalid length argument');
+    const lens = [15, 32, 6, 11, 87, 17, 1, 5, 3];
+    lens.forEach((l) => {
+      assert.strictEqual(pertyid(l).length, l);
+    });
   });
 });
